1a5791f1c949b58f4f58a19061eb620c6f0d87eb
srknclk5555 <55002831+srknclk5555@users.noreply.github.com>
2026-02-19 19:25:34 +0300

    Update eventController.js

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,13 +1,4 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-});
+const pool = require('../config/database');
 
 class EventController {
   // Tüm etkinlik loglarını getir
